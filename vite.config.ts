35aa70bc47eec293a4aa9a26f1d4935df1f917ca
leslyvj <leslychittattukara@gmail.com>
2025-11-12 11:51:34 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // ✅ Make sure this base matches your GitHub repo name
 export default defineConfig({
-  base: '/leslyvj.github.io/',
+  base: '/',
   plugins: [react()],
 })
